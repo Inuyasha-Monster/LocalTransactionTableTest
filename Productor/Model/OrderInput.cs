a90ecce3539e84bb2c039f0a2aa5187b9a0d30d2
dujl <dujl@tututech.net>
2018-04-13 22:09:56 +0800

    OrderInput.cs 添加注释

--- Productor/Model/OrderInput.cs
+++ Productor/Model/OrderInput.cs
@@ -6,28 +6,52 @@ using System.Threading.Tasks;
 
 namespace Productor.Model
 {
+    /// <summary>
+    /// 订单
+    /// </summary>
     public class OrderInput
     {
         public OrderInput()
         {
             Details = new List<OrderDetailInput>();
         }
+        /// <summary>
+        /// 用户
+        /// </summary>
         [Required]
         [StringLength(20)]
         public string AppUser { get; set; }
+        /// <summary>
+        /// 订单明细
+        /// </summary>
         public IList<OrderDetailInput> Details { get; set; }
 
     }
 
+    /// <summary>
+    /// 订单明细
+    /// </summary>
     public class OrderDetailInput
     {
+        /// <summary>
+        /// 商品SKU
+        /// </summary>
         [Required]
         [StringLength(50)]
         public string Sku { get; set; }
+        /// <summary>
+        /// 商品名称
+        /// </summary>
         [Required]
         [StringLength(50)]
         public string SkuName { get; set; }
+        /// <summary>
+        /// 数量
+        /// </summary>
         public int Quantity { get; set; }
+        /// <summary>
+        /// 单价
+        /// </summary>
         public decimal Price { get; set; }
     }
 }
--- Productor/Model/OrderOutput.cs
+++ Productor/Model/OrderOutput.cs
@@ -5,6 +5,9 @@ using System.Threading.Tasks;
 
 namespace Productor.Model
 {
+    /// <summary>
+    /// 订单
+    /// </summary>
     public class OrderOutput
     {
         public OrderOutput()
@@ -12,22 +15,60 @@ namespace Productor.Model
             Details = new List<OrderDetailOutput>();
         }
 
+        /// <summary>
+        /// 订单主键
+        /// </summary>
         public Guid Id { get; set; }
+        /// <summary>
+        /// 总价值
+        /// </summary>
         public decimal Amount { get; set; }
+        /// <summary>
+        /// 下单时间
+        /// </summary>
         public DateTime CreateTime { get; set; }
+        /// <summary>
+        /// 下单用户
+        /// </summary>
         public string AppUser { get; set; }
-
+        /// <summary>
+        /// 订单详情
+        /// </summary>
         public IList<OrderDetailOutput> Details { get; set; }
     }
 
+    /// <summary>
+    /// 订单详情
+    /// </summary>
     public class OrderDetailOutput
     {
+        /// <summary>
+        /// 详情主键
+        /// </summary>
         public Guid Id { get; set; }
+        /// <summary>
+        /// 订单主键
+        /// </summary>
         public Guid ParentId { get; set; }
+        /// <summary>
+        /// 商品SKU
+        /// </summary>
         public string Sku { get; set; }
+        /// <summary>
+        /// 商品名称
+        /// </summary>
         public string SkuName { get; set; }
+        /// <summary>
+        /// 数量
+        /// </summary>
         public int Quantity { get; set; }
+        /// <summary>
+        /// 单价
+        /// </summary>
         public decimal Price { get; set; }
+        /// <summary>
+        /// 创建时间
+        /// </summary>
         public DateTime CreateTime { get; set; }
     }
 }
