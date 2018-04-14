53dc111d123529c3a2bc8ba5f1cd8a3cd7facb38
dujl <dujl@tututech.net>
2018-04-14 23:45:37 +0800

    QuartzExtension.cs

--- Productor/Quartz/QuartzExtension.cs
+++ Productor/Quartz/QuartzExtension.cs
@@ -3,11 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace Productor.Quartz
 {
     public static class QuartzExtension
     {
+        public static void AddQuartz(this IServiceCollection service)
+        {
+
+        }
+
         public static void UseQuartz(this IApplicationBuilder app)
         {
 
