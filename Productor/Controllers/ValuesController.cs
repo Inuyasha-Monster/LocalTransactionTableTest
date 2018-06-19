f17166074a381a6cea0c0fcf1497856eb5c88825
djlnet <djlnet@17track.cn>
2018-06-19 11:59:02 +0800

    新增：增加测试错误，通过filebeat感知发现日志推送到reids->elk

--- Productor/Controllers/ValuesController.cs
+++ Productor/Controllers/ValuesController.cs
@@ -3,17 +3,33 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
 
 namespace Productor.Controllers
 {
     [Route("api/[controller]")]
     public class ValuesController : Controller
     {
+        private readonly ILogger<ValuesController> _logger;
+
+        public ValuesController(ILogger<ValuesController> logger)
+        {
+            _logger = logger;
+        }
+
         // GET api/values
         [HttpGet]
         public IEnumerable<string> Get()
         {
             return new string[] { "value1", "value2" };
+        }
+
+        [HttpGet]
+        [Route("testerror")]
+        public string TestErrorLog(int id)
+        {
+            _logger.LogError(new Exception("test"),"我是来测试error的");
+            return "testerror";
         }
 
         // GET api/values/5
